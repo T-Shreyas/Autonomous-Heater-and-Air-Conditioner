f0bc04b3a539cfa3a25565db47c0a433001942ca
T Shreyas <tshreyastshreyas@gmail.com>
2022-08-30 06:43:12 +0530

    Add files via upload

---
+++ Codes/Debug/makedep.mk
@@ -0,0 +1,12 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+ADC_driver.c
+
+DIO.c
+
+LCD.c
+
+temperature meter.c
+
